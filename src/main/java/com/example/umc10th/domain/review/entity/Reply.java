3ac6ca005c240e34c11025dcec34c4d1b7cad683
현경 <blue87083@gmail.com>
2026-04-27 01:25:17 +0900

    Feat: review 엔티티

--- src/main/java/com/example/umc10th/domain/review/entity/Reply.java
+++ src/main/java/com/example/umc10th/domain/review/entity/Reply.java
@@ -1,4 +1,22 @@
 package com.example.umc10th.domain.review.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "reply")
 public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content",nullable = false)
+    private String content;
 }
--- src/main/java/com/example/umc10th/domain/review/entity/Review.java
+++ src/main/java/com/example/umc10th/domain/review/entity/Review.java
@@ -1,4 +1,42 @@
 package com.example.umc10th.domain.review.entity;
 
-public class Review {
+import com.example.umc10th.domain.member.entity.Member;
+import com.example.umc10th.domain.mission.entity.Store;
+import com.example.umc10th.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.catalina.User;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "review")
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "contetn", nullable = false)
+    private String content;
+
+    @Column(name = "star", nullable = false)
+    private float star;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reply_id")
+    private Reply reply;
+
 }
